67a38ee99dc0e6c678f6c3b8c17d9b1b89e75c85
Vusal Ismayilov <woosal@protonmail.com>
2020-11-06 20:24:57 +0300

    CUSTOM

--- custom.py
+++ custom.py
@@ -2,6 +2,8 @@
 import time
 from selenium import webdriver  # for webdriver
 import os
+import cv2
+import numpy
 
 # Country Flags
 countryFlags = os.listdir("flags")
@@ -16,7 +18,7 @@ option.add_argument('headless')
 browser = webdriver.Chrome(chromeDriverPath, options=option)  #
 
 # Getting to https://iplocation.com/
-ipAddress = "78.183.107.236"
+ipAddress = "91.183.107.237"
 url = f"https://whatismyipaddress.com/ip/{ipAddress}"
 browser.get(url)
 time.sleep(2)
@@ -28,3 +30,7 @@ theCountry = countryLoc.text
 for i in countryFlags:
     if theCountry.lower() in i:
         print(i)
+        img = cv2.imread(f'flags/{i}', 1)
+        cv2.imshow('image', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
